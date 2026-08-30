503384ccbd6ed62c82415dedd6b1aeb66dbcddaa
kenan <kenaninaner@gmail.com>
2026-08-30 19:15:09 +0300

    fix(map): replace CartoCDN tiles with Google Maps Roadmap and ESRI Dark Gray Canvas to fix API Key Required watermark

--- src/components/map/MapLayout.jsx
+++ src/components/map/MapLayout.jsx
@@ -360,10 +360,10 @@ export default function MapLayout({ onReady, onOpenMenu, isMobile }) {
     return Object.keys(sessionsByDriver).filter(id => !deviceMappings[id] && id !== 'Bilinmeyen');
   }, [sessionsByDriver, deviceMappings]);
 
-  // ── Harita Tabanları (Orijinal ESRI Uydu Korunmuş + Canlı Trafik Eklenmiş) ──
+  // ── Harita Tabanları (API Key Hatası Vermeyen Google & ESRI Tabanları) ──
   const mapUrls = {
-    voyager: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-    darkmatter: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    voyager: 'https://mt{s}.google.com/vt/lyrs=m&hl=tr&x={x}&y={y}&z={z}',
+    darkmatter: 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}',
     satellite: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     traffic: 'https://mt{s}.google.com/vt/lyrs=y,traffic&hl=tr&x={x}&y={y}&z={z}',
   };
@@ -528,7 +528,7 @@ export default function MapLayout({ onReady, onOpenMenu, isMobile }) {
             <TileLayer
               key={mapStyle}
               url={mapUrls[mapStyle] || mapUrls.voyager}
-              subdomains={mapStyle === 'traffic' ? ['0', '1', '2', '3'] : ['a', 'b', 'c', 'd']}
+              subdomains={['traffic', 'voyager'].includes(mapStyle) ? ['0', '1', '2', '3'] : ['a', 'b', 'c', 'd']}
               maxZoom={20}
               maxNativeZoom={mapStyle === 'satellite' ? 18 : 19}
               keepBuffer={4}
